a7c7975054aaaf363c05c1e2cb0efd3b50402668
席升泉 <vectxi@gmail.com>
2016-10-17 20:08:42 +0800

    Update mini_ss.sh

--- mini/mini_ss.sh
+++ mini/mini_ss.sh
@@ -12,7 +12,7 @@ if [[ $EUID -ne 0 ]]; then
 fi
 cd /tmp
 rm -f shadowsocks_mini.tar.gz
-curl -O https://github.com/vectxi/miwifi-ss/master/mini/shadowsocks_mini.tar.gz
+curl -O https://raw.githubusercontent.com/vectxi/miwifi-ss/master/mini/shadowsocks_mini.tar.gz
 tar zxf shadowsocks_mini.tar.gz
 
 # install shadowsocks ss-redir to /data/usr/sbin
